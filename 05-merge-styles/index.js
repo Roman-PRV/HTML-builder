1aa7126717156bfeb7b1aaa84bfb0b2965cef184
R P <romanunicum@gmail.com>
2024-01-19 12:51:07 +0100

    refactor: do 05 by promises

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,33 +1,27 @@
 const fs = require('fs');
+const fsp = require('fs').promises;
 const path = require('path');
 
-// const util = require('util');
-// const appendFile = util.promisify(fs.appendFile);
-
 const srcFolderPath = path.join(__dirname, 'styles');
-const dstFilePath = path.join(__dirname, 'project-dist', 'bundle.css');
+const styleDstFile = path.join(__dirname, 'project-dist', 'bundle.css');
 
-// create bundle file
-fs.writeFile(dstFilePath, '', (err) => {
-  if (err) throw err;
-});
+async function mergeStylesByOrden(fileArray, outputFile) {
+  await fsp.writeFile(outputFile, '');
+  for (let i = 0; i < fileArray.length; i++) {
+    const data = await fsp.readFile(fileArray[i], 'utf8');
+    await fsp.writeFile(outputFile, data, { flag: 'a' });
+  }
+}
 
 fs.readdir(srcFolderPath, (err, files) => {
-  if (err) console.log(err);
-  else {
-    filesCSS = files.filter((file) => path.extname(file) === '.css');
-    filesCSS.forEach((file) => {
-      fs.readFile(path.join(srcFolderPath, file), 'utf8', (err, data) => {
-        if (err) {
-          console.log(err);
-        } else {
-          fs.appendFile(dstFilePath, data, (err) => {
-            if (err) {
-              console.log(err);
-            }
-          });
-        }
-      });
-    });
+  if (err) {
+    console.log(err);
+  } else {
+    const fullPathFiles = files
+      .filter((file) => path.extname(file.toLowerCase()) === '.css')
+      .map((file) => path.join(srcFolderPath, file));
+    mergeStylesByOrden(fullPathFiles, styleDstFile)
+      .then(() => console.log(`+ CSS style has been merged`))
+      .catch(console.error);
   }
 });
